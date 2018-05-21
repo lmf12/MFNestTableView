9e77b150d0c8ea3c31fb586fa17152bb4f90e073
Lyman <limianfeng12@gmail.com>
2018-05-22 00:37:17 +0800

    fix bug：某些情况下，contentView中的点击事件不能响应

--- MFNestTableView/MFAllowGestureEventPassTableView.m
+++ MFNestTableView/MFAllowGestureEventPassTableView.m
@@ -14,6 +14,41 @@
 
 @implementation MFAllowGestureEventPassTableView
 
+- (instancetype)init {
+    
+    self = [super init];
+    if (self) {
+        [self commonInit];
+    }
+    return self;
+}
+
+- (instancetype)initWithFrame:(CGRect)frame {
+    
+    self = [super initWithFrame:frame];
+    if (self) {
+        [self commonInit];
+    }
+    return self;
+}
+
+- (instancetype)initWithCoder:(NSCoder *)aDecoder {
+    
+    self = [super initWithCoder:aDecoder];
+    if (self) {
+        [self commonInit];
+    }
+    return self;
+}
+
+#pragma mark - private methods
+
+- (void) commonInit {
+    // 在某些情况下，contentView中的点击事件会被panGestureRecognizer拦截，导致不能响应，
+    // 这里设置cancelsTouchesInView表示不拦截
+    self.panGestureRecognizer.cancelsTouchesInView = NO;
+}
+
 #pragma mark - UIGestureRecognizerDelegate
 
 // 返回YES表示可以继续传递触摸事件，这样两个嵌套的scrollView才能同时滚动
